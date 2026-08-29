e693a077522f7f78cd86175eccda2d1af45448e2
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-08-29 16:28:54 +0300

    2 answer

--- Program.cs
+++ Program.cs
@@ -15,5 +15,10 @@ class program
         book.Pages = 10;
         object obj = book;
         Console.WriteLine(obj);
-    }
+     //2 answer
+    Console.WriteLine(book.ToString());
+        Console.WriteLine(book.Equals(book));
+        Console.WriteLine(book.GetHashCode());
+        Console.WriteLine(book.GetType());
+}
 }
